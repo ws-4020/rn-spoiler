8428b413b056f43d6d2bc5705fc2d6bad6b889c4
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-10-09 02:17:06 +0900

    Fix lint suggestions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   // https://github.com/expo/expo/tree/master/packages/eslint-config-universe
   // React Native向けの設定と、TypeScriptの型チェックを設定しています。
-  extends: ["universe/native", "universe/shared/typescript-analysis"],
+  extends: ['universe/native', 'universe/shared/typescript-analysis'],
   overrides: [
     {
-      files: ["*.ts", "*.tsx", "*.d.ts"],
+      files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
     },
   ],
