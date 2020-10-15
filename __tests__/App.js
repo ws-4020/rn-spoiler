3b4f6bad15cbcdea8f1f08d95db66e64d968363b
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-10-15 21:40:09 +0900

    Setup Jest to run successfully

--- __tests__/App.js
+++ __tests__/App.js
@@ -4,6 +4,9 @@ import renderer from 'react-test-renderer';
 
 import App from '../src/App.tsx';
 
+// https://github.com/facebook/jest/issues/6434#issuecomment-525576660
+jest.useFakeTimers();
+
 // Note: test renderer must be required after react-native.
 
 it('renders correctly', () => {
