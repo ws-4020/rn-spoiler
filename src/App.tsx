e52f7b47c5c017b25059af306130b00d83e91c02
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-10-15 20:42:46 +0900

    [done installation] import react-native-gesture-handler and wrap app with NavigationContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import * as React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
