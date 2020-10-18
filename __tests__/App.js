693947780bcb77c7a96f4528654acb9e8899328a
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-10-19 08:35:12 +0900

    🌱: Add React Navigation examples (simple tab and stack navigators)

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,11 +1,12 @@
 import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
+// Note: test renderer must be required after react-native.
+import {act, create} from 'react-test-renderer';
 
 import App from '../src/App.tsx';
 
-// Note: test renderer must be required after react-native.
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  await act(async () => {
+    create(<App />);
+  });
 });
---
+++ src/example/components/pages/form/InputForm2Screen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Button, Text} from 'react-native-elements';
+
+import {WizardScreenDefinition} from '../../parts/WizardScreenDefinition';
+import {FormWizardParams, FormWizardScreenProps} from './FormWizardNavigator';
+
+const Definition: WizardScreenDefinition<FormWizardParams> = {
+  name: 'Input2',
+  component: Component,
+  options: {
+    headerTitle: '🖋 Input 2',
+  },
+};
+export default Definition;
+
+function Component({navigation}: FormWizardScreenProps<typeof Definition.name>) {
+  return (
+    <View>
+      <Text>Input Form 2 Component</Text>
+      <Button onPress={() => navigation.navigate('Confirm')}>Next</Button>
+    </View>
+  );
+}
