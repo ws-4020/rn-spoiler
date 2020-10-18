7f4739553664d3901b6e3639c57ca827fdd04f53
In a Paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-10-18 17:21:28 +0900

    🌱: Keep awake while developing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import {activateKeepAwake} from 'expo-keep-awake';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  // 開発中は画面がスリープしないようにしておきます。
+  if (__DEV__) {
+    activateKeepAwake();
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
