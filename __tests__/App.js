d9d26a4ec309e8a0ed64497d64acb0dbbef98f53
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-10-18 21:59:32 +0900

    Fix test failure (use `act`)

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,14 +1,12 @@
 import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
+// Note: test renderer must be required after react-native.
+import {act, create} from 'react-test-renderer';
 
 import App from '../src/App.tsx';
 
-// https://github.com/facebook/jest/issues/6434#issuecomment-525576660
-jest.useFakeTimers();
-
-// Note: test renderer must be required after react-native.
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  await act(async () => {
+    create(<App />);
+  });
 });
