938f8fef68eac639aa381b89f88f1a4d9a7b8236
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-11-06 05:30:09 +0900

    Implement each Icon in the same way

--- src/example/components/basics/CameraIcon.tsx
+++ src/example/components/basics/CameraIcon.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {Icon as RNEIcon} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
 
 import {SpecificIconType} from './SpecificIconType';
 
 // React Native Elementsを使用したアイコンのサンプル
-const Icon = ({color = '#9D8CB8', ...props}: SpecificIconType) => {
-  return <RNEIcon name="camera" type="font-awesome" color={color} {...props} />;
+export default ({color = '#9D8CB8', ...props}: SpecificIconType) => {
+  return <Icon name="camera" type="font-awesome" color={color} {...props} />;
 };
-
-export default Icon;
--- src/example/components/basics/PencilIcon.tsx
+++ src/example/components/basics/PencilIcon.tsx
@@ -4,8 +4,6 @@ import {Icon} from 'react-native-elements';
 import {SpecificIconType} from './SpecificIconType';
 
 // React Native Elementsを使用したアイコンのサンプル
-const PencilIcon = ({color = '#9D8CB8', ...props}: SpecificIconType) => {
+export default ({color = '#9D8CB8', ...props}: SpecificIconType) => {
   return <Icon name="pencil" type="font-awesome" color={color} {...props} />;
 };
-
-export default PencilIcon;
