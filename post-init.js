7bfaff369d18e5973a55bf7c029fe45b37f75d18
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-11-15 01:38:00 +0900

    ✨: Initialize the project after creating from template
    * Add PersonalAccount.xcconfig (AB#4663)
    * Generate debug.keystore (AB#5019)

--- post-init.js
+++ post-init.js
@@ -1,15 +1,60 @@
 #!/usr/bin/env node
 
-const copyIosPersonalAccountConfig = async () => {};
-const generateAndroidDebugKeystore = async () => {};
+const fs = require("fs");
+const fsp = require("fs/promises");
+const { spawn } = require("child_process");
 
-const main = async () => {
-  await Promise.all([
-    copyIosPersonalAccountConfig(),
-    generateAndroidDebugKeystore(),
-  ]);
+const copyIosPersonalAccountConfig = async () => {
+  const file = "ios/PersonalAccount.xcconfig";
+  await fsp.copyFile(`${file}.template`, `${file}`, fs.constants.COPYFILE_EXCL).then(() => {
+    console.log("  ✔ Copy configuration file to set your Apple ID");
+  });
 };
 
-// Promise is handled by caller.
-// noinspection JSIgnoredPromiseFromCall
-main();
+const generateAndroidDebugKeystore = async () => {
+  const file = "android/app/debug.keystore";
+  if (fs.existsSync(file)) {
+    await fsp.unlink(file);
+  }
+  const command = {
+    command: "keytool",
+    args: [
+      "-genkey",
+      "-v",
+      "-keystore",
+      "android/app/debug.keystore",
+      "-storepass",
+      "android",
+      "-alias",
+      "androiddebugkey",
+      "-keypass",
+      "android",
+      "-dname",
+      "'CN=Android Debug,O=Android,C=US'",
+    ],
+  };
+  return new Promise((resolve, reject) => {
+    let stdout = "";
+    const process = spawn(command.command, command.args, { shell: true });
+    process.stdout.on("data", (chunk) => (stdout += chunk));
+    process
+      .on("error", () => reject(stdout))
+      .on("close", (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(stdout);
+        }
+      });
+  }).then(() => console.log("  ✔ Generate debug.keystore file"));
+};
+
+const main = async () => {
+  await Promise.all([copyIosPersonalAccountConfig(), generateAndroidDebugKeystore()]);
+};
+
+main().catch((reason) => {
+  console.log(); // 標準出力上でログを見やすくするために、一行改行しています。
+  console.error(reason);
+  process.exit(1);
+});
