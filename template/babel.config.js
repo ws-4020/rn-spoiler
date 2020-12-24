aae4b7de9024ca90b023197e4b79959f363c2d64
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-12-24 19:30:06 +0900

    ✨: Use absolute import

--- template/babel.config.js
+++ template/babel.config.js
@@ -2,5 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        },
+      ],
+    ],
   };
 };
