5d46acfc0abf5470d19728dc79741e7205d5fad9
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-12-24 19:25:31 +0900

    ✨: Add 'react-hooks/recommended' rule to eslint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   // https://github.com/expo/expo/tree/master/packages/eslint-config-universe
   // React Native向けのチェックと、TypeScriptの型情報を利用したチェックを設定しています。
-  extends: ['universe/native', 'universe/shared/typescript-analysis'],
+  extends: ['universe/native', 'plugin:react-hooks/recommended'],
   overrides: [
     {
-      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+      // TypeScript向けに型情報を利用したチェックを設定しています。
+      extends: ['universe/shared/typescript-analysis', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
       files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
         project: './tsconfig.json',
