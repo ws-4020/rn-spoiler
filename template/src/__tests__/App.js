109289f8b3fc5f98ce6291f6cd61e477407bacd0
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2020-12-25 10:59:26 +0900

    ♻: Change baseUrl to `src`

--- template/src/__tests__/App.js
+++ template/src/__tests__/App.js
@@ -1,8 +1,9 @@
 import 'react-native';
-import {App} from 'App';
 import React from 'react';
 // Note: test renderer must be required after react-native.
 import {act, create} from 'react-test-renderer';
+
+import {App} from '../App';
 
 it('renders correctly', async () => {
   await act(async () => {
