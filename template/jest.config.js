1d6b95f1a91356a19344266ceab18c48e069009d
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-01-23 04:52:24 +0900

    ✔: Write tests in TypeScript, move test file, run as ios and android platform

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
   // __mocks__ディレクトリをjestディレクトリ内に配置したいので、ルートディレクトリにjestを設定しています。
-  roots: ['<rootDir>', 'jest'],
+  roots: ['<rootDir>/src', 'jest'],
   setupFiles: ['<rootDir>/jest/setup/react-navigation.js'],
-  // 変更のあったファイルがわかりやすいように、テストファイルの横にスナップショットファイルが配置されるようにしています。
-  snapshotResolver: '<rootDir>/jest/snapshotResolver.js',
 };
