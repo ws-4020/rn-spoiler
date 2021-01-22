fc76d9143895cc75e01689a724692d94f9b0626d
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-01-23 01:42:47 +0900

    🖋: Refine documents and comments

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  // https://github.com/expo/expo/tree/master/packages/eslint-config-universe
-  // React Native向けのチェックと、TypeScriptの型情報を利用したチェックを設定しています。
+  // universe/native: https://github.com/expo/expo/tree/master/packages/eslint-config-universe
+  // react-hooks: https://ja.reactjs.org/docs/hooks-rules.html
   extends: ['universe/native', 'plugin:react-hooks/recommended'],
   overrides: [
     {
-      // TypeScript向けに型情報を利用したチェックを設定しています。
+      // universe/shared/typescript-analysis: https://github.com/expo/expo/tree/master/packages/eslint-config-universe
+      // recommended-requiring-type-checking: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
       extends: ['universe/shared/typescript-analysis', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
       files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
