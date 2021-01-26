bc825a28699557fcefdf3df6dec4d6ebfafeb511
in-a-paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-01-26 19:48:57 +0900

    ✨: Remove expo-keep-awake

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,13 +1,8 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {activateKeepAwake} from 'expo-keep-awake';
 import {RootStackNav} from 'navigation';
 import React from 'react';
 
 export const App = () => {
-  // 開発中は画面がスリープしないようにしておきます。
-  if (__DEV__) {
-    activateKeepAwake();
-  }
   return (
     <NavigationContainer>
       <RootStackNav />
