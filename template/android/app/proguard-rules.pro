6c0f913afa3500731537cdd5bdce1da1393140c9
In-a-Paddy <44867563+in-a-paddy@users.noreply.github.com>
2021-12-02 13:45:45 +0900

    ⬆: Upgrade Expo SDK to 43 (#89)
    ## 💣 BREAKING CHANGES 💣
    
    - Update Xcode to 13
      - Xcode 12 fails to build production app
    
    ## ✅ What's done
    
    - [x] `expo upgrade`
    - [x] Apply changes of [expo-template-bare-minimum](https://github.com/expo/expo/commits/sdk-43/templates/expo-template-bare-minimum)
    - [x] Apply changes of [React Native Upgrade Helper](https://react-native-community.github.io/upgrade-helper/?from=0.63.4&to=0.64.2)
    - [x] Fix Xcode warnings
      - Automatically Select Architectures
    - [x] Update Jest mock settings of React Navigation
    - [x] Update ProGuard configuration
      - Keep React Native Turbomodules classes
      - Keep `expo.modules.ExpoModulesPackageList`
    - [x] Remove unused `expo-updates`
    - [x] Add `arrowParens` rule of Prettier
    - [x] Fix patch files to create hands-on app

--- template/android/app/proguard-rules.pro
+++ template/android/app/proguard-rules.pro
@@ -8,3 +8,11 @@
 #   http://developer.android.com/guide/developing/tools/proguard.html
 
 # Add any project specific keep options here:
+
+# React Native ReanimatedからTurbomoduleを使うための設定
+# https://docs.swmansion.com/react-native-reanimated/docs/2.2.0/installation
+-keep class com.facebook.react.turbomodule.** { *; }
+
+# ExpoModulesPakage.ktから、自動生成されたクラスを参照するためにクラス名を利用しているので、クラス名が変わるとアプリが起動しなくなる。
+# https://github.com/expo/expo/blob/sdk-43/packages/expo/android/src/main/java/expo/modules/ExpoModulesPackage.kt#L22
+-keep class expo.modules.ExpoModulesPackageList { *; }
--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -7,10 +7,10 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-import expo.modules.splashscreen.singletons.SplashScreen;
-import expo.modules.splashscreen.SplashScreenImageResizeMode;
+import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
 
@@ -22,28 +22,26 @@ public class MainActivity extends ReactActivity {
     // https://github.com/software-mansion/react-native-screens/issues/17
     // Discard any Activity state persisted during the Activity restart process, to avoid inconsistencies that lead to crashes.
     super.onCreate(null);
-    // SplashScreen.show(...) has to be called after super.onCreate(...)
-    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
   }
 
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "main";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "main";
+  }
 
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
-    }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegateWrapper(
+        this,
+        new ReactActivityDelegate(this, getMainComponentName()) {
+          @Override
+          protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+          }
+        });
+  }
 }
