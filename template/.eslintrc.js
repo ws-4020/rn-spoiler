189ad729528b0eccc8fda1d769db61790ccc22b1
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-06-09 15:16:05 +0900

    Add eslint-plugin-deprecation and eslint-plugin-jest

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -4,12 +4,23 @@ module.exports = {
   extends: ['universe/native', 'plugin:react-hooks/recommended'],
   overrides: [
     {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
       // universe/shared/typescript-analysis: https://github.com/expo/expo/tree/master/packages/eslint-config-universe
       // recommended-requiring-type-checking: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
       extends: ['universe/shared/typescript-analysis', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
-      files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
         project: './tsconfig.json',
+      },
+    },
+    {
+      files: ['*.test.ts', '*.test.tsx'],
+      // https://github.com/jest-community/eslint-plugin-jest#shareable-configurations
+      extends: ['plugin:jest/recommended'],
+      rules: {
+        // you should turn the original rule off *only* for test files
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
+        '@typescript-eslint/unbound-method': 'off',
+        'jest/unbound-method': 'error',
       },
     },
     {
@@ -22,6 +33,16 @@ module.exports = {
             checksVoidReturn: false,
           },
         ],
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      plugins: ['deprecation'],
+      rules: {
+        'deprecation/deprecation': 'error', // or "error" to have stricter rule
+      },
+      parserOptions: {
+        project: './tsconfig.json',
       },
     },
     {
