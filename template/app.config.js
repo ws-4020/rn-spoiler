9274f9c4843e9113991c1cfec7fa2a11a704ad93
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2023-03-14 15:09:44 +0900

    fixup! feat: Proguard Rule 追加 (ExpoModulesPackageList)

--- template/app.config.js
+++ template/app.config.js
@@ -43,7 +43,7 @@ module.exports = ({config}) => {
           android: {
             extraProguardRules: `
 # ExpoModulesPakage.ktから、自動生成されたクラスを参照するためにクラス名を利用しているので、クラス名が変わるとアプリが起動しなくなる。
-# https://github.com/expo/expo/blob/sdk-43/packages/expo/android/src/main/java/expo/modules/ExpoModulesPackage.kt#L22
+# https://github.com/expo/expo/blob/sdk-46/packages/expo/android/src/main/java/expo/modules/ExpoModulesPackage.kt#L23
 -keep class expo.modules.ExpoModulesPackageList { *; }
 `,
           },
