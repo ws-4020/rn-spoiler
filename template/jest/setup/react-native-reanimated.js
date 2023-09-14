b23b13dc4fedb7d11c0b418475d91ea5c57d9327
Takashi Sugimoto <sugimoto.takashi@tis.co.jp>
2023-09-14 16:47:11 +0900

    test: react-native-reanimated v2のjestUtilsパス変更への対応
    > Cannot find module 'react-native-reanimated/lib/reanimated2/jestUtils' from 'jest/setup/react-native-reanimated.js'

--- template/jest/setup/react-native-reanimated.js
+++ template/jest/setup/react-native-reanimated.js
@@ -1,2 +1,2 @@
-// https://docs.swmansion.com/react-native-reanimated/docs/guide/testing/
-require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+// https://docs.swmansion.com/react-native-reanimated/docs/guides/testing/
+require('react-native-reanimated').setUpTests();
